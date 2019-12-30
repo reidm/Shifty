0bfc668b43eb62b2202a03781ff49b8e3037fe1d
Reid Miller <sweetreid@gmail.com>
2019-12-30 01:42:37 -0500

    Initial working code.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Shifty
-Arduino firmware for race shifters
+Arduino firmware for race shifters. Supports linear hall sensors for greater shifting accuracy than microswitches or magnetic switches.
+
+Gear changes are semi-latched, to prevent your sim race car from popping out of gear. This is aimed to reduce driver sadness.
+
+## Configuration
+           USB
+      -------------
+      | TX0   RAW |    
+      | RXI   GND | G
+    G | GND   RST |
+      | GND   VCC | Power
+      |  2    A3  |
+      |  3    A2  | In
+   In |  4    A1  | In
+      |  5    A0  | In  
+   In |  6    15  |
+      |  7    14  |
+   In |  8    16  |
+   In |  9    10  | In
+      -------------
+
+## Sensors
+Any old linear hall sensor rated at 5v should do. I'm using the 49E: https://sensing.honeywell.com/index.php?ci_id=50359
+
+Pin out
+ 1    2    3
+VCC   G    Out
+
+## PCB
+
+The sensors are mounted on PCBs. The PCBs have mounting holes 4.5 cm apart.
+Top row: +5v (yellow wire)
+2nd row: G (white wire)
+3rd row (cut in middle trace): outer sensor output (red)
+4th row (cut in middle trace): inner sensor output (red)
---
+++ Shifty.ino
@@ -0,0 +1,90 @@
+/*
+  Shifty.ino
+
+  Copyright (c) 2019, Reid Miller
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+//#define DEBUG 1
+
+#include <Joystick.h>
+Joystick_ Joystick;
+
+#define SENSOR_COUNT 8
+#define NO_BUTTON -1
+
+const int reading_threshold = 90;
+const int stale_count_threshold = 85;
+const int sensor_pin[SENSOR_COUNT] = {A3, A1, A0, A2, A9, A7, A8, A6};
+const int button_map[SENSOR_COUNT] = {0, 2, 4, 10, 1, 3, 5, 7};
+
+int sensor_minimum[SENSOR_COUNT] = {1000, 1000, 1000, 1000,
+  1000, 1000, 1000, 1000};
+int button_active = NO_BUTTON;
+int stale_count = 0;
+
+void setup() {
+  int sensor_num;
+  for(sensor_num = 0; sensor_num< SENSOR_COUNT; sensor_num++){
+    pinMode(sensor_pin[sensor_num], INPUT);
+    sensor_minimum[sensor_num] = analogRead(sensor_pin[sensor_num]);
+  }
+  Joystick.begin();
+  #ifdef DEBUG
+  Serial.begin(9600);
+  #endif
+}
+
+void loop() {
+  int sensor_num;
+  int value = 0;
+  int flag = 0;
+  for(sensor_num = 0; sensor_num < SENSOR_COUNT; sensor_num++){
+      value = analogRead(sensor_pin[sensor_num]);
+      if(value < sensor_minimum[sensor_num])
+        sensor_minimum[sensor_num] = value;
+      value -= sensor_minimum[sensor_num];
+
+      if(value > reading_threshold){
+        flag = 1;
+        stale_count = 0;
+        if(button_active != NO_BUTTON && button_active != sensor_num){
+          Joystick.setButton(button_map[button_active], LOW);
+        }
+        Joystick.setButton(button_map[sensor_num], HIGH);
+        button_active = sensor_num;
+      }
+
+      #ifdef DEBUG
+      Serial.print(value);
+      if (sensor_num < SENSOR_COUNT-1){
+        Serial.print(" / ");
+      } else{
+        Serial.println("");
+        delay(10);
+      }
+      #endif
+  }
+
+  if(!flag)
+    stale_count +=1;
+
+  if(stale_count >= stale_count_threshold){
+    Joystick.setButton(button_map[button_active], LOW);
+    stale_count = 0;
+  }
+
+
+}
